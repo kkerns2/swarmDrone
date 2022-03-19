9b3b967c7a96a34a1a5b9239b6e0041129eabd35
Akash Samlal <triforcemaster345@gmail.com>
2022-03-19 16:57:24 -0400

    Update swarm nav clients

---
+++ nav_point_publisher/drone2_client.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+from dataclasses import replace
+import rospy
+import actionlib
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+
+def movebase_drone2_client(x, y):
+    client2 = actionlib.SimpleActionClient('/drone2/move_base', MoveBaseAction)
+    
+    client2.wait_for_server()
+    
+    client2.cancel_goal()
+   
+    goal2 = MoveBaseGoal()
+   
+    goal2.target_pose.header.frame_id = "map"
+   
+    goal2.target_pose.header.stamp = rospy.Time.now()
+
+    goal2.target_pose.pose.position.x = x
+    
+    goal2.target_pose.pose.position.y = y
+   
+    goal2.target_pose.pose.orientation.w = 1.0
+    
+    client2.send_goal(goal2)
+
+    wait = client2.wait_for_result()
+
+    if not wait:
+        rospy.logerr("Action server is not available")
+        rospy.signal_shutdown("Action server is not available")
+    else:
+        return client2.get_result()
+
+if __name__=='__main__':
+
+    with open('/home/mcp/catkin_ws/src/nav_point_publisher/drone2_waypoints.txt', 'r') as f:
+        lines_2 = f.readlines()
+
+    drone2_coordinates = []
+
+    for i in range(len(lines_2)):
+        lines_2[i] = lines_2[i].replace('\n', '')
+        drone2_coordinates.append(lines_2[i].split(" "))
+        for j in range(2): 
+            drone2_coordinates[i][j] = float(drone2_coordinates[i][j])
+
+    try:
+        #add coordinate logic here
+        rospy.init_node('drone2_client_py')
+        
+        drone2_count = 0
+        
+        while drone2_count < len(drone2_coordinates):
+    
+            drone2_result = movebase_drone2_client(drone2_coordinates[drone2_count][0], drone2_coordinates[drone2_count][1])
+    
+            if drone2_result:
+                rospy.loginfo("Drone 2: Goal executed")
+                drone2_count +=1
+            else:
+                rospy.loginfo("Drone 2: Something went wrong")
+                break
+
+    except rospy.ROSInterruptException:
+        rospy.loginfo("Swarm Drone Navigation Broadcast Finished")
+
---
+++ nav_point_publisher/drone4_client.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+from dataclasses import replace
+import rospy
+import actionlib
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+
+def movebase_drone4_client(x, y):
+    client4 = actionlib.SimpleActionClient('/drone4/move_base', MoveBaseAction)
+    
+    client4.wait_for_server()
+    
+    client4.cancel_goal()
+   
+    goal4 = MoveBaseGoal()
+   
+    goal4.target_pose.header.frame_id = "map"
+   
+    goal4.target_pose.header.stamp = rospy.Time.now()
+
+    goal4.target_pose.pose.position.x = x
+    
+    goal4.target_pose.pose.position.y = y
+   
+    goal4.target_pose.pose.orientation.w = 1.0
+    
+    client4.send_goal(goal4)
+
+    wait = client4.wait_for_result()
+
+    if not wait:
+        rospy.logerr("Action server is not available")
+        rospy.signal_shutdown("Action server is not available")
+    else:
+        return client4.get_result()
+
+if __name__=='__main__':
+
+    with open('/home/mcp/catkin_ws/src/nav_point_publisher/drone4_waypoints.txt', 'r') as f:
+        lines_4 = f.readlines()
+
+    drone4_coordinates = []
+
+    for i in range(len(lines_4)):
+        lines_4[i] = lines_4[i].replace('\n', '')
+        drone4_coordinates.append(lines_4[i].split(" "))
+        for j in range(2): 
+            drone4_coordinates[i][j] = float(drone4_coordinates[i][j])
+
+    try:
+        #add coordinate logic here
+        rospy.init_node('drone4_client_py')
+        
+        drone4_count = 0
+        
+        while drone4_count < len(drone4_coordinates):
+    
+            drone4_result = movebase_drone4_client(drone4_coordinates[drone4_count][0], drone4_coordinates[drone4_count][1])
+    
+            if drone4_result:
+                rospy.loginfo("Drone 1: Goal executed")
+                drone4_count +=1
+            else:
+                rospy.loginfo("Drone 1: Something went wrong")
+                break
+
+    except rospy.ROSInterruptException:
+        rospy.loginfo("Swarm Drone Navigation Broadcast Finished")
+
