3809430d39378f68e8622bc051bd8fef27f8647e
Akash Samlal <triforcemaster345@gmail.com>
2022-04-04 16:42:46 -0400

    old test main

--- bash_script/detector.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-rosrun detector yolo_multi.py drone1 drone2 &
-rosrun detector yolo_multi.py drone3 drone4 &
--- bash_script/drone3_takeoff.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source /opt/ros/noetic/setup.bash
-source ~/testing_ws/devel/setup.bash
-
-rostopic pub /drone3/takeoff std_msgs/Empty "{}"
